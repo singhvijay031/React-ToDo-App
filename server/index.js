e42c756793b3804a9f817a4ff63c01d30179834d
Bijay <bijaymahato031@gmail.com>
2024-06-30 22:04:22 +0545

    added files

--- server/index.js
+++ server/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
-const app = express;
+const app = express();
+require("dotenv").config();
 
+app.use(express.json());
 
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
