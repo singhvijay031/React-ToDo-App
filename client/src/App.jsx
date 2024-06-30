40c75557c193fc556257962d2feb928c648892f1
Bijay <bijaymahato031@gmail.com>
2024-07-01 00:07:02 +0545

    updated css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    </>
+    <div className="container">
+      <h1>ToDo App</h1>
+    </div>
   );
 }
 
