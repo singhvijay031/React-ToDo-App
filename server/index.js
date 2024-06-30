265fb2c4f65a07f3207089d4975fd3cafc6840d0
Bijay <bijaymahato031@gmail.com>
2024-06-30 22:20:32 +0545

    added database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Home Page");
-});
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Database Connectd..");
+  })
+  .catch(() => {
+    console.log("Can't connect to Database...");
+  });
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
