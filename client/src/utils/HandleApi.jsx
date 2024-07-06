a3c4e073303577d3fae21a81b1f7ea9cd55dbde0
Bijay <bijaymahato031@gmail.com>
2024-07-06 18:27:47 +0545

    api changed

--- client/src/utils/HandleApi.jsx
+++ client/src/utils/HandleApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:8000";
+const baseUrl = "https://todo-dsdcqu9ey-bijay-mahatos-projects.vercel.app/";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
